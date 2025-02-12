d6b85e484f751322515d14b53a3ff0cdb0aefa44
DhyanMehta <152066218+DhyanMehta@users.noreply.github.com>
2025-02-12 12:49:23 +0530

    project start

---
+++ EduSphere/app/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native"
+
+const index =  ()=>{
+
+  return(
+    <view>
+      <text>
+        Hello Welocme to dhyans app
+      </text>
+    </view>
+  )
+
+}
+export default index
